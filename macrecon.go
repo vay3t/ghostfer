b84aba117f8688c76d069a4f6a71205b57536e22
vay3t <vay3t@github.com>
2019-07-10 14:28:09 -0400

    fix funct filter

--- macrecon.go
+++ macrecon.go
@@ -17,7 +17,7 @@ import (
 var (
 	iface		string
 	snapshotLen	int32  = 1024
-	promiscuous	bool   = false
+	promiscuous	bool   = true
 	err		error
 	timeout		time.Duration = 30 * time.Second
 	handle		*pcap.Handle
@@ -25,10 +25,11 @@ var (
 )
 
 func main() {
+	fmt.Println("[+] Starting MAC Recon...")
 	iface := flag.String("iface", "wlan0", "Interface use to sniffing")
 	flag.Parse()
 	hosts = make(map[string]bool)
-	
+
 	handle, err = pcap.OpenLive(*iface, snapshotLen, promiscuous, timeout)
 	if err != nil {log.Fatal(err) }
 	defer handle.Close()
@@ -46,22 +47,22 @@ func main() {
 
 func printPacketInfo(packet gopacket.Packet) {
 	arp := packet.Layer(layers.LayerTypeARP).(*layers.ARP)
-	if arp.Operation == 2 {
-		macSrc := net.HardwareAddr(arp.SourceHwAddress)
-		ipSrc := net.IP(arp.SourceProtAddress)
-		
-		buffer := bytes.Buffer{}
-		buffer.WriteString(macSrc.String())
-		buffer.WriteString(" - ")
-		buffer.WriteString(ipSrc.String())
-		stringCompleto := buffer.String()
-		
-		if _, ok := hosts[stringCompleto]; ok {
-			fmt.Print()
-		}else{
-			hosts[stringCompleto] = true
-			fmt.Println(stringCompleto)
-		}
+	macSrc := net.HardwareAddr(arp.SourceHwAddress)
+	ipSrc := net.IP(arp.SourceProtAddress)
+
+	buffer := bytes.Buffer{}
+	buffer.WriteString(macSrc.String())
+	buffer.WriteString(" - ")
+	buffer.WriteString(ipSrc.String())
+	stringCompleto := buffer.String()
+
+	if _, ok := hosts[stringCompleto]; ok {
+		return
+	} else if ipSrc.String() == "0.0.0.0" {
+		return
+	} else{
+		hosts[stringCompleto] = true
+		fmt.Println(stringCompleto)
 	}
 
 	// Check for errors
